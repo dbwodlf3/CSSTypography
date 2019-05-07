cd2ae50a0dcb7a6346d6d6d17f36c9b37e49e976
Ryu <dbwodlf3@naver.com>
2019-05-07 15:55:23 +0900

    URL으로 Exmaples 반환하기

--- home/viewer/urls.py
+++ home/viewer/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
 from . import views
 
+
 urlpatterns = [
     path('', views.home),
+    path('examples/<str:content>/', views.viewer)
 ]
--- home/viewer/views.py
+++ home/viewer/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseNotFound
+from django.conf import settings
+import os
 
 # Create your views here.
 
 def home(request,*args, **kwargs):
     return render(request, "index.html")
+
+def viewer(request, *args, **kwargs):
+    filePath = os.path.join(settings.TEMPLATES[0]["DIRS"][0],"contents","%s.html"%(kwargs["content"]))
+    if os.path.isfile(filePath):
+        return render(request, "contents/%s.html"%kwargs["content"])
+    else:
+        return HttpResponseNotFound("not Found!")

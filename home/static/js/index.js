46aae5463fc242092446b6b3168faafc2c53e8b3
Ryu <dbwodlf3@naver.com>
2019-05-30 10:02:16 +0900

    Layouts error fix

--- home/static/js/index.js
+++ home/static/js/index.js
@@ -1,11 +1,12 @@
 function makeEmptyCell (event){
     //lineItemMaximum의 스크린의 크기가 300px보다 작으면 값이 0이 됨.
     //n의 값의 범위는 NaN,1부터 lineItemMaximum 값 까지임.
-    var lineItemMaximum = Math.floor(window.innerWidth/300);
-    var itemNumbers = document.getElementsByClassName("card1").length;
-    var lastLineItemNumbers = itemNumbers % lineItemMaximum;
+    var lineItemMaximum = Math.floor(window.innerWidth/330); // 한 라인에 들어갈 수 있는 아이템의 최대 갯수. 이건 종속적임.. 나중에 어떻게 수정되어야 할듯. index.css에서 card의 마진값도 추가해서 계산해야함. 스크롤바가 생길 경우에, 스크롤바가 공간을 차지하면서 오작동함.. 여유값을 주자.
+    var itemNumbers = document.getElementsByClassName("card1").length; // 현재 페이지의 아이템의 갯수.
+    var lastLineItemNumbers = itemNumbers % lineItemMaximum; // 마지막 줄에 있는 아이템의 갯수.
     var n = lineItemMaximum - lastLineItemNumbers;
 
+    alert(lineItemMaximum);
 
     //삭제
     var removeTarget = document.getElementsByClassName("emptyCell")
@@ -20,9 +21,9 @@ function makeEmptyCell (event){
     if(n != lineItemMaximum && n != NaN && itemNumbers > lineItemMaximum){
         var target = document.getElementsByClassName("cardFlexWrapper")[0];
         for(var i=0;i<n;i++){
-            var tempElement =document.createElement("div",{"class":"emptyCell"});
+            var tempElement =document.createElement("div");
             tempElement.className = "emptyCell"
-            
+
             tempArray.push(tempElement)
         }
         for(var i=0;i<n;i++){
@@ -33,5 +34,5 @@ function makeEmptyCell (event){
         //alert(testMessage);
     }
 }
-window.onload = makeEmptyCell;
+//window.onload = makeEmptyCell; //createCell이 추가 되었다. 이제 콘텐츠가 추가된 다음에 정렬을 한번 해주도록 하자.
 window.onresize = makeEmptyCell;
